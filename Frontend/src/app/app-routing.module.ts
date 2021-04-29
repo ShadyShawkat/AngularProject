d57f4a61435386bf4303b6d70391f64660a59818
Mohamed Mohiey <mohhiey@gmail.com>
2021-04-29 10:13:21 +0200

    fixed conflicts

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,17 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { RegisterComponent } from './components/users/register/register.component';
 
-const routes: Routes = [
-  {path:'register',component:RegisterComponent},
 
-=======
 import { GenreComponent } from './components/genre/genre.component';
 
 const routes: Routes = [
-  {path:'genres', component: GenreComponent}
->>>>>>> 2db5eba5b1945fd0733f89dc2aeeb871a40aadce
+  {path:'genres', component: GenreComponent},
+  {path:'register',component:RegisterComponent},
+  
+
 ];
 
 @NgModule({
